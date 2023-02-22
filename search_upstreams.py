27d86a0fe211b88b3576588f241e34bbc8f3560d
Trevor Kirby <trevor@Trevors-MacBook-Pro-4.local>
2023-02-22 09:51:08 -0800

    identify key ASNs to deploy ASPA from

---
+++ search_upstreams.py
@@ -0,0 +1,19 @@
+import os
+import json
+
+INFILE = 'the_internet.json'
+OUTFILE = 'top_level_asn.json'
+
+if os.path.isfile(INFILE):
+    with open(INFILE, 'r') as infile:
+        asn_network_data = json.loads(infile.read())
+
+top_level_asns = []
+
+for asn in asn_network_data:
+    if len(asn['upstreams']) == 0:
+        print(f'{asn["asn"]}\t{asn["country"]}\t{asn["rir"]}')
+        top_level_asns.append(asn['asn'])
+
+with open(OUTFILE, 'w') as outfile:
+    outfile.write(json.dumps(top_level_asns))
