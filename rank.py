a6041d5ee5a013dc85ada38778bdbb07feb551ac
TianWong <tianwong02@gmail.com>
2023-03-14 14:34:45 -0700

    ranking according to top level ASNs

--- rank.py
+++ rank.py
@@ -6,12 +6,15 @@ def rank(input_file, output_file):
     with open(input_file, 'r') as infile:
         asn_network_data = json.loads(infile.read())
 
+    with open("top_level_asn.json", 'r') as topfile:
+        top_data = json.loads(topfile.read())
+
     top_list = []
     G = nx.Graph()
     
     for asn_entry in asn_network_data:
         asn = asn_entry["asn"]
-        if len(asn_entry["upstreams"]) == 0:
+        if asn in top_data:
             top_list.append(asn)
         neighbors = set(asn_entry["peers"] + asn_entry["upstreams"])
         G.add_node(asn)
@@ -29,8 +32,6 @@ def rank(input_file, output_file):
     
     with open(output_file, 'w') as outfile:
         json.dump(list(asn_network_data), outfile, indent=2, separators=(',',':'))
-    import code
-    code.interact(local=locals())
 
 
 if __name__ == "__main__":
